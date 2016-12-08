91403629a27c6402e19465cac05bb779272eeb23
dhruvkmr96 <dhruvkmratiitk@gmail.com>
2016-12-08 13:20:10 +0530

    Create MyWebBrowser_Main.java

---
+++ MyWebBrowser_Main.java
@@ -0,0 +1,8 @@
+import javax.swing.JFrame;
+public class MyWebBrowser_Main {
+
+	public static void main(String args[]){
+		MyWebBrowser browser = new MyWebBrowser();
+		browser.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
